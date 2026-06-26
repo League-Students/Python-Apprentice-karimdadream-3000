afd3269cbddd5a9ee9eb15021258401de003ee48
League Student <student@jointheleague.org>
2026-06-25 18:12:05 -0700

    Automated commit

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -18,7 +18,7 @@ tina.speed(2)                            # Move at a moderate speed, not too fas
 
 def draw_polygon('3'):
 
-    angle = ...                          # Calculate angle from number of sides
+    angle = '3'                          # Calculate angle from number of sides
     
     for i in range(...):                 # Loop through the number of sides
         ...                              # Move tina forward by the forward distance

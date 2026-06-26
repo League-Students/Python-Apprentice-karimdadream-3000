c6d6330e57fcb1a2ad7d2dde54b0a740bfbae0c5
League Student <student@jointheleague.org>
2026-06-25 17:05:08 -0700

    Automated commit

--- lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/30_Turtle_Tricks.py
@@ -27,7 +27,7 @@ tina.end_fill()
 
 tina.goto(0, -75)
 
-tina.color('blue)                       # Set the color of tina to red
+tina.color('blue')                       # Set the color of tina to red
 tina.begin_fill()
 tina.circle(75)
 tina.end_fill()

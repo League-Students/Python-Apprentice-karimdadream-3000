73ad50ceee7958a6b262bb2fc2540ed25b76ee86
League Student <student@jointheleague.org>
2026-06-25 17:37:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -22,7 +22,7 @@ tina.speed(2)                           # Move at a moderate speed, not too fast
  
 for i in range(5):
     tina.forward(200)
-    tina.right(360/5)
+    tina.left(360/5)
 
 
 turtle.exitonclick()                    

42d976c290204a85e84eb66326df63f15303e162
League Student <student@jointheleague.org>
2026-06-25 18:19:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -18,7 +18,7 @@ tina.speed(2)                            # Move at a moderate speed, not too fas
 
 def draw_polygon(sides):
 
-    angle = '3'                          # Calculate angle from number of sides
+    angle = 360/sides                          # Calculate angle from number of sides
     
     for i in range(''):                 # Loop through the number of sides
         200                              # Move tina forward by the forward distance

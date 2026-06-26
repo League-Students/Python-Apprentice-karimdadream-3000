d4bfb2676f3073a62d3127af5e00637b21926550
League Student <student@jointheleague.org>
2026-06-25 18:20:02 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -24,7 +24,7 @@ def draw_polygon(sides):
         200                              # Move tina forward by the forward distance
         60                             # Turn tina left by the left turn
 
-draw_polygon3, 50, "red"')                        # Draw a square
+draw_polygon (3)                       # Draw a square
 
                                       # Move tina to another spot on the screen
 
